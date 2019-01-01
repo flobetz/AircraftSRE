442137d332e8fbd0c1be79cf2b5f7a32446fcd91
Betz, Florian <florian.betz@daimler.com>
2019-01-01 23:42:11 +0100

    FEAT: added Dockerfile for application image

--- scripts/startStack.sh
+++ scripts/startStack.sh
@@ -3,9 +3,5 @@
 # start postgres container
 docker run --name some-postgres -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres
 
-# compile go app
-cd /Users/flobetz/Documents/projects/go/AircraftSRE
-go build
-
-# run go app
-./AircraftSRE
+# start app container
+docker run -d -p 80:80 flights:latest

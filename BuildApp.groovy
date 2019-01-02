9ed0e6597cdbaeb7abcfd929a16dc6b6803601a8
Betz, Florian <florian.betz@daimler.com>
2019-01-02 15:54:54 +0100

    FEATURE: added Jenkinsfile for building the flightOperator App

--- BuildApp.groovy
+++ BuildApp.groovy
@@ -3,15 +3,14 @@
 node("master"){
 
     stage("Checkout") {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']],
-                  doGenerateSubmoduleConfigurations: false,
-                  extensions: [[$class: 'LocalBranch'], [$class: 'CleanBeforeCheckout'],
-                               [$class: 'IgnoreNotifyCommit']],
-                  submoduleCfg: [],
-                  userRemoteConfigs: [[credentialsId: 'github-credentials', url: 'https://github.com/flobetz/AircraftSRE.git']]])
+        checkout scm
     }
 
     stage("Build") {
-        sh "ls -lsa"
+        sh "docker build -t flightOperator:latest ."
+    }
+
+    stage("Push") {
+        sh "docker push flightOperator:latest"
     }
 }
